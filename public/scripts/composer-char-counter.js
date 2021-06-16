d2a7e6421bc88c9bbd3917adbfd864a1d27a7ca9
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-16 07:27:23 +0000

    text input class name edited

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   console.log('document is ready');
-  $('#tweet-text').on('input', function() {
+  $('#new-text').on('input', function() {
     const button = $('button');
-    const textInput = $('#tweet-text').val();
-    const counter = $('#tweet-text').parent().children('.submitLine').children('.counter');
+    const textInput = $('#new-text').val();
+    const counter = $('#new-text').parent().children('.submitLine').children('.counter');
     const result = 140 - textInput.length;
     if (result < 0) {
       counter.css('color', 'red');
