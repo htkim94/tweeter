155b87dcb452bba4e95c8aa38f6b6ac20bd9b21a
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-16 22:32:14 +0000

    implemented  loadTweets using ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1623701690105
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1623788090105
-  }
-]
 
 const createTweetElement = (tweet) => {
   const { name, avatars, handle } = tweet.user;
@@ -73,5 +49,18 @@ const renderTweets = (tweets) => {
 }
 
 $(document).ready(() => {
-  renderTweets(tweetData);
+  const loadTweets = () => {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    .then((tweets) => {
+      renderTweets(tweets);
+    })
+  }
+
+  loadTweets();
+
+  $('form').submit(function (event) {
+    event.preventDefault();
+    console.log(event);
+    console.log($(this).serialize());
+  })
 });
