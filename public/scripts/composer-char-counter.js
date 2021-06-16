449abf027c823b51b6c7795c0f64320a568930c8
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-16 05:41:46 +0000

    added new tweet button disable feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   console.log('document is ready');
   $('#tweet-text').on('input', function() {
+    const button = $('button');
     const textInput = $('#tweet-text').val();
     const counter = $('#tweet-text').parent().children('.submitLine').children('.counter');
     const result = 140 - textInput.length;
     if (result < 0) {
       counter.css('color', 'red');
+      button.prop('disabled', true);
     } else {
       counter.css('color', '');
+      button.prop('disabled', false);
     }
     counter[0].innerHTML = result;
   })
